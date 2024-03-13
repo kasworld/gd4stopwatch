fbe91996677f1c5945f84683aa64ae8c9eb5822d
SeukWon Kang <kasworld@gmail.com>
2024-03-13 14:46:17 +0900

    scroll to new node

--- main.gd
+++ main.gd
@@ -11,7 +11,7 @@ func _ready() -> void:
 	$ScrollContainer.size = vp_rect.size
 	StopwatchContainer.size = vp_rect.size
 	var msgrect = Rect2( vp_rect.size.x * 0.0 ,vp_rect.size.y * 0.5 , vp_rect.size.x * 1.0 , vp_rect.size.y * 0.22 )
-	$TimedMessage.init(msgrect, tr("multi stopwatch 3.5.0"))
+	$TimedMessage.init(msgrect, tr("multi stopwatch 3.6.0"))
 	$TimedMessage.show_message("click time to start/stop, long press to reset ",1)
 	add_stopwatch()
 
@@ -21,8 +21,9 @@ func add_stopwatch()->void:
 	sw.started.connect(recv_stopwatch_started)
 	StopwatchContainer.add_child(sw)
 	tr_list.append(sw)
-	sw.grab_focus()
-	#$ScrollContainer.ensure_control_visible(sw)
+	#sw.grab_focus()
+	await get_tree().process_frame
+	$ScrollContainer.ensure_control_visible(sw)
 
 func recv_stopwatch_started(n:int)->void:
 	if n +1 == tr_list.size():

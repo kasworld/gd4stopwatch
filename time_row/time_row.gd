92f13ab698dc7e4573944ea1a977e126cf67435a
SeukWon Kang <kasworld@gmail.com>
2024-03-16 17:26:34 +0900

    arrange for android export

--- time_row/time_row.gd
+++ time_row/time_row.gd
@@ -2,6 +2,9 @@ extends FlowContainer
 
 class_name TimeRow
 
+@onready var time_preset_menu = $HBoxContainer/TimePresetMenu
+@onready var intedit = $HBoxContainer/IntEdit
+
 signal started(n:int) # emit when 1st start
 signal ended(n:int) # emit when count down over 0
 
@@ -30,16 +33,16 @@ var index :int
 func init(idx :int, fsize :int)->void:
 	index = idx
 	$ToggleButton.theme.default_font_size = fsize
-	$IntEdit.init(idx, fsize, TickLib.tick2stri)
-	$IntEdit.set_limits( 0,true,0,99,false)
-	$IntEdit.value_changed.connect(_on_edit_value_changed)
+	intedit.init(idx, fsize, TickLib.tick2stri)
+	intedit.set_limits( 0,true,0,99,false)
+	intedit.value_changed.connect(_on_edit_value_changed)
 	$TimeRecorder.init(idx,fsize, TickLib.tick2str)
 	$TimeRecorder.started.connect(_on_tr_started)
 	$TimeRecorder.overrun.connect(_on_tr_overrun)
-	$TimePresetMenu.theme.default_font_size = fsize/3
-	$TimePresetMenu.get_popup().theme = Theme.new()
-	$TimePresetMenu.get_popup().theme.default_font_size = fsize/4
-	$TimePresetMenu.get_popup().index_pressed.connect(_on_timepreset_menu_index_pressed)
+	time_preset_menu.theme.default_font_size = fsize/2.5
+	time_preset_menu.get_popup().theme = Theme.new()
+	time_preset_menu.get_popup().theme.default_font_size = fsize/2.5
+	time_preset_menu.get_popup().index_pressed.connect(_on_timepreset_menu_index_pressed)
 	make_preset_popup()
 	mode_stopwatch()
 
@@ -51,29 +54,27 @@ func _on_tr_overrun(v:float)->void:
 	ended.emit(index)
 
 func _on_edit_value_changed(n :int)->void:
-	var v = $IntEdit.get_value()
+	var v = intedit.get_value()
 	$TimeRecorder.set_initial_sec(v)
 
 func make_preset_popup()->void:
 	for i in timepreset_list.size():
-		$TimePresetMenu.get_popup().add_item(timepreset_list[i][0],i)
+		time_preset_menu.get_popup().add_item(timepreset_list[i][0],i)
 
 func _on_timepreset_menu_index_pressed(sel :int)->void:
 	var v = timepreset_list[sel][1]
-	$IntEdit.set_init_value(v)
+	intedit.set_init_value(v)
 	$TimeRecorder.set_initial_sec(v)
 
 func mode_stopwatch()->void:
 	$ToggleButton.text = "%d:⏱" % index
 	$TimeRecorder.set_stopwatch()
-	$IntEdit.visible = false
-	$TimePresetMenu.visible = false
+	$HBoxContainer.visible = false
 
 func mode_countdowntimer()->void:
 	$ToggleButton.text = "%d:⏳" % index
-	$TimeRecorder.set_initial_sec($IntEdit.get_value())
-	$IntEdit.visible = true
-	$TimePresetMenu.visible = true
+	$TimeRecorder.set_initial_sec(intedit.get_value())
+	$HBoxContainer.visible = true
 
 func _on_check_button_toggled(toggled_on: bool) -> void:
 	if toggled_on:
